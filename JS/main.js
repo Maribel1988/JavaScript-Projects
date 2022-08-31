b415daa49806db1b7b3180b04631b64a7d19fb99
Maribel1988 <102556943+Maribel1988@users.noreply.github.com>
2022-08-31 03:22:59 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+//my window alert code
+    window.alert("Hello, World!");
+//document write code
+document.write('hello, world!');
+// assigning variable
+var A = "Good Morning!";
+
+document.write(A);
+
+//concatenate 
+let text1 = "Sun";
+let text2 = "rise";
+let result = text1.concat(text2);
+document.getElementById("concat1").innerHTML = result;
+//fontcolor
+let text = "Wake Up!";
+
+result = "<span style='color:green'>" + text + "</span>";
+document.getElementById("font1").innerHTML = result;
+
+

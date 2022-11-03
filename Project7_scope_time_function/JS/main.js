d6284617c5a173fa988418eda95f6ac1d78e1959
Maribel1988 <102556943+Maribel1988@users.noreply.github.com>
2022-11-03 11:42:09 +0000

    Add files via upload

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -26,3 +26,40 @@ function my_Numbers_2() {
 }
 my_Numbers_1();
 my_Numbers_2();
+
+/*Time_function */
+function get_Date() {
+    var greeting;
+
+    if (new Date().getHours() < 18){
+        greeting = 'Good evening';
+    }
+   
+        document.getElementById("Greeting").innerHTML = greeting + " How are you today?";
+    
+}
+
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+function Time_function() {
+    var Time = new Date().getHours();
+    var Greeting;
+    if (Time < 12 == Time >0) {
+        Greeting = "Good morning!";
+    }
+    else if (Time>12 == Time < 18){
+        Greeting = "Good afternoon.";
+      }  else {
+            Greeting = "Good evening.";
+    } document.getElementById("Time_of_day").innerHTML = Greeting
+    
+}
+
